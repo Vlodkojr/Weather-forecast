3254bcdfe33abc965b3538c26328438b7e8c2e40
Volodymyr Romanovskyi <volodymyrrom1@gmail.com>
2021-03-16 14:28:29 +0200

    Add enterClick

--- src/InputAndButton.js
+++ src/InputAndButton.js
@@ -7,10 +7,17 @@ export default function InputAndButton ({ setTown }) {
     const handleClick = () => {
         setTown(valueRef.current.value);                       
     }  
+
+    const enterClick = (e) => { 
+        if(e.charCode === 13) {           
+            e.preventDefault();
+            setTown(valueRef.current.value);
+        }
+    }
     
     return (
         <div className="inputButton">
-            <input type = "text" placeholder = "Enter a town" ref={valueRef} />  
+            <input type = "text" placeholder = "Enter a town" ref={valueRef} onKeyPress={enterClick}/>  
             <button onClick={handleClick}>Search</button>  
         </div>
     )
